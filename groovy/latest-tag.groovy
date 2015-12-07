0ae5308a64231be06a5ebead664149f275da1b2d
Amos Wenger <fasterthanlime@gmail.com>
2015-12-07 13:56:51 +0100

    switch to doing more with groovy, less with conditional build steps

--- groovy/latest-tag.groovy
+++ groovy/latest-tag.groovy
@@ -1,21 +1,23 @@
 
-System.getenv().each {
-  println it
-}
+pattern = "[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*"
 
-pattern = (['[0-9]' * 2 + '*'] * 3).join "." 
 out = "git log --tags=$pattern --pretty=format:%d".execute().text
-latest = (out =~ pattern).collect().sort(false) { a, b ->
+versions = (out =~ pattern)
+latest = versions.collect().sort(false) { a, b ->
   [a, b]*.tokenize('.')*.collect { it as int }.with { u, v ->
     [u, v].transpose().findResult{ x, y -> x <=> y ?: null } ?: u.size() <=> v.size()
   }
 }[-1]
 
-store = new File('latest-tag.txt')
-if (store.exists() && store.text == latest) {
-  throw new RuntimeException("No new tags to build")
+props = "JENKINS_TAG = v$latest"
+
+latest_tag = new File('latest_tag.properties')
+revision = new File('revision.properties')
+
+if (latest_tag.exists() && latest_tag.text == props) {
+  revision.text = "JENKINS_TAG = **"
+} else {
+  revision.text = props
 }
 
-store.text = latest
-return [TAG: latest]
-
+  latest_tag.text = props
